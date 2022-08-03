8b230de4782ea257c24b65572011e9b10a04f2b7
zhangz1w3nbeatbox <zhangz1w3nbbx@163.com>
2022-08-03 16:16:35 +0800

    Initial commit

--- TikTok_Api/src/main/java/com/zzw/Application.java
+++ TikTok_Api/src/main/java/com/zzw/Application.java
@@ -3,11 +3,13 @@ package com.zzw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.zzw.mapper")
 @ComponentScan(basePackages = {"com.zzw","org.n3r.idworker"})
+@EnableMongoRepositories
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
